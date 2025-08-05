4459c1093ab3b670d6c4c3de9bf810b6ecf4c525
Jeff Ponteras <jeffponteras.pmc@gmail.com>
2025-08-05 11:10:26 +0800

    fix

--- app/Models/SavedContactOfficial.php
+++ app/Models/SavedContactOfficial.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
-use App\Models\official;
+use App\Models\Official;
 
 class SavedContactOfficial extends Model
 {
