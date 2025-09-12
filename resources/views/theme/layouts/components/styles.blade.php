5f251cfd1e45efb790ed6b99ff25b5d9bca58fa0
Jeff Ponteras <jeffponteras.pmc@gmail.com>
2025-09-12 13:22:15 +0800

    typo

--- resources/views/theme/layouts/components/styles.blade.php
+++ resources/views/theme/layouts/components/styles.blade.php
@@ -6,8 +6,8 @@
     <!-- Font Imports -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Sofia+Sans+Extra+Condensed:ital,wght@0,1..1000;1,1..1000&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
     <!-- Stylesheets
     ============================================= -->
@@ -575,6 +575,7 @@
         }
 
         /*new font for menu*/
+        #header nav.primary-menu.with-arrows .menu-container .menu-item .menu-link > div,
         #header nav.primary-menu.with-arrows .menu-container .menu-item .menu-link {
             font-family: 'Sofia Sans Extra Condensed' !important;
             font-size: 24px;
